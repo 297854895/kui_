a0024e07b1525f9ed5b0ff89256e396403e12aa3
Kecheng Zhu <kecheng.zhu@socialcredits.cn>
2017-01-24 17:59:16 +0800

    update

---
+++ components/TextEditor/Pane.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="k-textEditor-pane">
+    <k-button-group v-for="item of section">
+      <button @click="PaneClick(item_)" v-for="item_ of item" v-if="itemOptions[item_].kind === 'button'" :class="`k k-textEditor-bt k-button k-button-default k-radius k-button-size-small ${ itemOptions[item_].stat ? 'k-textEditor-bt-active' : ''}`">
+        <i :class="`fa ${itemOptions[item_].icon}`" style="margin: 0px;"></i>
+      </button>
+      <button v-else-if="itemOptions[item_].kind === 'pane'" :class="`k k-textEditor-bt k-textEditor-pane-bt k-button k-button-default k-radius k-button-size-small ${ itemOptions[item_].stat ? 'k-textEditor-bt-active' : ''}`">
+        <span class="k-textEditor-pane-btin" style="line-height: 22px;">
+          <i :class="`fa ${itemOptions[item_].icon}`" style="margin: 0px;"></i>
+        </span>
+      </button>
+      <span v-else>select</span>
+    </k-button-group>
+  </div>
+</template>
+<script>
+  import kButtonGroup from '../ButtonGroup/ButtonGroup';
+
+  export default{
+    name: 'k-textEditor-pane',
+    components: {
+      'k-button-group': kButtonGroup
+    },
+    methods: {
+      PaneClick(key) {
+        if(!this.$data.itemOptions[key]) return;
+        this.$data.itemOptions[key].stat = !this.$data.itemOptions[key].stat;
+        this.$parent.$emit(`k-textEditor-pane-${this.$parent._uid}`, {key: key, options: null});
+      }
+    },
+    data() {
+      return {
+        itemOptions: {
+          Redo: {
+            icon: 'fa-rotate-left',
+            kind: 'button',
+            stat: false
+          },
+          Undo: {
+            icon: 'fa-rotate-right',
+            kind: 'button',
+            stat: false
+          },
+          ForeColor: {
+            icon: 'fa-font',
+            kind: 'pane',
+            stat: false
+          },
+          Bold: {
+            icon: 'fa-bold',
+            kind: 'button',
+            stat: false
+          },
+          Italic: {
+            icon: 'fa-italic',
+            kind: 'button',
+            stat: false
+          },
+          Header: {
+            icon: 'fa-header',
+            kind: 'button',
+            stat: false
+          },
+          StrikeThrough: {
+            icon: 'fa-strikethrough',
+            kind: 'button',
+            stat: false
+          },
+          Underline: {
+            icon: 'fa-underline',
+            kind: 'button',
+            stat: false
+          },
+          Subscript: {
+            icon: 'fa-subscript',
+            kind: 'button',
+            stat: false
+          },
+          Superscript: {
+            icon: 'fa-superscript',
+            kind: 'button',
+            stat: false
+          },
+          Indent: {
+            icon: 'fa-indent',
+            kind: 'button',
+            stat: false
+          },
+          Outdent: {
+            icon: 'fa-outdent',
+            kind: 'button',
+            stat: false
+          },
+          JustifyLeft: {
+            icon: 'fa-align-left',
+            kind: 'button',
+            stat: false
+          },
+          JustifyRight: {
+            icon: 'fa-align-right',
+            kind: 'button',
+            stat: false
+          },
+          JustifyCenter: {
+            icon: 'fa-align-center',
+            kind: 'button',
+            stat: false
+          },
+          JustifyFull: {
+            icon: 'fa-align-justify',
+            kind: 'button',
+            stat: false
+          },
+          Paste: {
+            icon: 'fa-paste',
+            kind: 'button',
+            stat: false
+          },
+          Copy: {
+            icon: 'fa-copy',
+            kind: 'button',
+            stat: false
+          },
+          Cut: {
+            icon: 'fa-cut',
+            kind: 'button',
+            stat: false
+          },
+          Save: {
+            icon: 'fa-save',
+            kind: 'button',
+            stat: false
+          },
+          InsertOrderedList: {
+            icon: 'fa-list-ol',
+            kind: 'button',
+            stat: false
+          },
+          InsertUnorderedList: {
+            icon: 'fa-list-ul',
+            kind: 'button',
+            stat: false
+          },
+          Table: {
+            icon: 'fa-table',
+            kind: 'button',
+            stat: false
+          },
+          Heading: {
+            icon: 'fa-header',
+            kind: 'button',
+            stat: false
+          },
+          FontSize: {
+            kind: 'select',
+            stat: false
+          }
+        },
+        section: [
+          ['Bold', 'ForeColor', 'FontSize']
+        ],
+        PaneOptions: {
+          ForeColor: {
+            style: '#333',
+            viewColor: '#333',
+            show: false,
+            color: [['fff', 'f2f2f2', 'd8d8d8', 'bfbfbf', 'a5a5a5', '7f7f7f'],
+                    ['000', '7f7f7f', '595959', '3f3f3f', '262626', '0c0c0c'],
+                    ['eeece1', 'ddd9c3', 'c4bd97', '938953', '494429', '1d1b10'],
+                    ['1f497d', 'c6d9f0', '8db3e2', '548dd4', '17365d', '0f243e'],
+                    ['4f81bd', 'dbe5f1', 'b8cce4', '9fb3d7', '366092', '244061'],
+                    ['c0504d', 'f2dcdb', 'e5b9b7', 'd99694', '953734', '632423'],
+                    ['9bbb59', 'ebf1dd', 'd7e3bc', 'c3d69b', '76923c', '4f6128'],
+                    ['8064a2', 'e5e0ec', 'ccc1d9', 'b2a2c7', '5f497a', '3f3151'],
+                    ['4bacc6', 'dbeef3', 'b7dde8', '92cddc', '31859b', '205867'],
+                    ['f79646', 'fdeada', 'fbd5b5', 'fac08f', 'e36c09', '974806']]
+          }
+        },
+      }
+    }
+  }
+</script>
--- components/TextEditor/TextEditor.vue
+++ components/TextEditor/TextEditor.vue
@@ -1,112 +1,29 @@
 <template>
   <div class="k-textEditor" ref="kEditor">
-    <div class="k-textEditor-pane" ref="textEditorPane">
-      <k-button-group v-for="each in options">
-        <button v-if="!each_.hidePane" v-for="(each_, key) in each" class="k k-button k-button-default k-button-size-small k-radius" @click="emit(key, null)">
-          <i :class="`fa ${iconOptions[key]}`" style="margin: 0px;">
-            {{iconOptions[key] ? '' : each_}}
-          </i>
-        </button><button v-else class="k k-button k-button-default k-button-size-small k-radius k-textEditor-hidePaneBt" @click="selectPaneClick(key)">
-          <i role="action" :class="`fa ${iconOptions[key]}`" style="margin: 0px;"></i><i role="pane" class="fa fa-angle-down k-textEditor-hidePaneBt-i" style="line-height: 22px;font-size: 12px;"></i>
-        </button>
-      </k-button-group>
-    </div>
+    <k-textEditor-pane />
     <iframe class="k-textEditor-iframe" ref="textEditor" :style="`height:${ifrHeight ? ifrHeight : ''}`"></iframe>
     <div class="k-textEditor-footer"></div>
   </div>
 </template>
 <script>
-  import ButtonGroup from '../ButtonGroup/ButtonGroup';
-  import Select from '../Select/Select';
+  import textEditorPane from './Pane';
 
   export default {
     name: 'k-textEditor',
+    components: {
+      'k-textEditor-pane': textEditorPane
+    },
     props: {
       height: {
         type: String,
         default: '400px'
       },
-      iconOptions: {
-        type: Object,
-        default: () => {
-          return {
-            Redo: 'fa-rotate-left',
-            Undo: 'fa-rotate-right',
-            Font: 'fa-font',
-            Bold: 'fa-bold',
-            Italic: 'fa-italic',
-            Header: 'fa-header',
-            StrikeThrough: 'fa-strikethrough',
-            Underline: 'fa-underline',
-            Subscript: 'fa-subscript',
-            Superscript: 'fa-superscript',
-            Indent: 'fa-indent',
-            Outdent: 'fa-outdent',
-            JustifyLeft: 'fa-align-left',
-            JustifyRight: 'fa-align-right',
-            JustifyCenter: 'fa-align-center',
-            JustifyFull: 'fa-align-justify',
-            Paste: 'fa-paste',
-            Copy: 'fa-copy',
-            Cut: 'fa-cut',
-            Save: 'fa-save',
-            InsertOrderedList: 'fa-list-ol',
-            InsertUnorderedList: 'fa-list-ul',
-            Table: 'fa-table',
-            Heading: 'fa-header'
-          }
-        }
-      },
-      options: {
-        type: Object,
-        default: () => {
-          return {
-            one: {
-              Redo: true,
-              Undo: true
-            },
-            two: {
-              Font: {
-                hidePane: true,
-              },
-              Bold: true,
-              Italic: true,
-              Underline: true,
-              StrikeThrough: true,
-              Subscript: true,
-              Superscript: true
-            },
-            three: {
-              Indent: true,
-              Outdent: true,
-              JustifyLeft: true,
-              JustifyRight: true,
-              JustifyCenter: true,
-              JustifyFull: true,
-            },
-            four: {
-              Paste: true,
-              Copy: true,
-              Cut: true,
-              Save: true
-            },
-            five: {
-              Table: true,
-            },
-            six: {
-              InsertHorizontalRule: `—`,
-              InsertOrderedList: true,
-              InsertUnorderedList: true,
-            }
-          };
-        }
-      }
-    },
-    components: {
-      'k-button-group': ButtonGroup
     },
     data() {
       return {
+        styleNodeMap: {
+          B: 'Bold'
+        },
         ifrHeight: this.height,
         EditorTemplate: `<html>
                     <head>
@@ -120,27 +37,16 @@
                       </style>
                     </head>
                     <body style="word-break:break-all;"></body>
-                  </html>`,
-        hidePane: {
-          Font: true,
-        }
+                  </html>`
       }
     },
     mounted() {
       this.initEditor();
+      this.$on(`k-textEditor-pane-${this._uid}`, (data) => {
+        this.emit(data.key, data.options);
+      });
     },
     methods: {
-      selectPaneClick(key) {
-        if (event.target.getAttribute('role') === 'pane') {
-          console.log("showPane");
-        }else {
-          if (event.target.parentNode.className.indexOf('k-textEditor-hidePaneBt-active') > -1) {
-            event.target.parentNode.className = event.target.parentNode.className.replace('k-textEditor-hidePaneBt-active', '');
-          }else {
-            event.target.parentNode.className += ' k-textEditor-hidePaneBt-active';
-          }
-        }
-      },
       emit(cmd, options) {
         var win = this.$refs.textEditor.contentDocument || this.$refs.textEditor.contentWindow.document;
         win.execCommand(cmd, false, options);
@@ -163,7 +69,25 @@
         doc.contentEditable = true;
         doc.body.appendChild(this.createElement('p', '<br>'));
         doc.body.focus();
+        doc.body.onclick = this.currentStyle;
+        // doc.body.onselect = this.currentStyle;
         // doc.onkeydown = this.insertP;
+      },
+      recursionNode(node, direction) {
+        if (!node.children || node.children.length <= 0) return;
+      },
+      currentStyle(evt) {
+        const Node = evt.target;
+        if (Node.tagName === 'BODY') return;
+        if (Node.tagName === 'P') {
+          console.log(Node.lastChild.tagName);
+          return;
+        }
+        for (let key in this.$data.styleNodeMap) {
+          if (Node.tagName === key) {
+            console.log(key);
+          }
+        }
       },
       insertP(evt) {
         if (evt.which === 13) {
