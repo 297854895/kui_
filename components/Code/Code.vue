9b6207ce778fc0a993c5ba4a5e043d25b02ca9bd
Kecheng Zhu <kecheng.zhu@socialcredits.cn>
2017-02-13 11:48:57 +0800

    new code commponent

---
+++ components/Code/Code.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>This is code</span>
+</template>
+<script>
+  import hljs from 'highlightjs';
+
+  export default{
+    name: 'k-code',
+    directives: {
+      highLightCode: function(el, bind) {
+        if (binding.value) {
+           let value = null
+           if (typeof(binding.value) === "string") {
+               value = binding.value
+           } else {
+               value = JSON.stringify(binding.value, null, 4)
+           }
+           el.innerHTML = hljs.highlight("json", value, true).value
+       }
+      }
+    }
+  }
+</script>
