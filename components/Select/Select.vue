43df28dc6307ffae5eeb007d881de8e61781bc69
= <=>
2017-02-26 07:36:29 +0800

    update

--- components/Select/Select.vue
+++ components/Select/Select.vue
@@ -1,6 +1,13 @@
 <template>
   <span :class="`k k-select ${radius === 'yes' ? 'k-radius' : '' }`" @click="toggleSelect">
-    <p><input v-if="input === 'readonly'" type="text" readonly="readonly" :value="current.text" :style="`width:${width}`"/><input v-model="inputValue" :placeholder="placeholder" type="text" v-else :style="`width:${width}`"/><span class="k-select-btn"><i class="fa fa-angle-down k-select-btn-click" :style="`${selectShow ? 'transform: rotate(180deg)' : ''}`"></i></span></p>
+    <p>
+      <input v-if="type === 'mut'" type="text" readonly="readonly" :value="this.handleMutSelected(this.$data.mutSelected)" :style="`width:${width}`" :placeholder="placeholder"/>
+      <input v-else-if="input === 'readonly'" type="text" readonly="readonly" :value="current.text" :style="`width:${width}`"/>
+      <input v-model="inputValue" :placeholder="placeholder" type="text" v-else :style="`width:${width}`"/>
+      <span class="k-select-btn">
+        <i class="fa fa-angle-down k-select-btn-click" :style="`${selectShow ? 'transform: rotate(180deg)' : ''}`"></i>
+      </span>
+    </p>
   </span>
 </template>
 <script>
@@ -31,6 +38,18 @@
       placeholder: {
         type: String,
         default: ''
+      },
+      selected: {
+        type: Object,
+        default: null
+      },
+      mutSelectedArr: {
+        type: Array,
+        default: null
+      },
+      type: {
+        type: String,
+        default: 'single'
       }
     },
     watch: {
@@ -53,8 +72,20 @@
       }
     },
     data () {
+      const mutSelected = [];
+      if (this.type === 'mut') {
+        if (this.select) {
+          const arr = [];
+          this.select.forEach((item) => {
+             if (item.status) {
+               mutSelected.push(item);
+             }
+           });
+        }
+      }
       return {
-        current: this.select ? this.select[0] : '',
+        mutSelected: mutSelected,
+        current: this.selected ? this.selected : this.select ? this.select[0] : '',
         inputValue: this.select ? this.select[0].text : '',
         selectShow: false,
         menu: ''
@@ -66,6 +97,15 @@
           this.$data.menu = vue_;
         }
       });
+      if (this.type === 'mut') {
+        this.$on(`k-select-menu-update-mut-select-${this._uid}`, (data) => {
+          this.$data.mutSelected = data;
+          if (this.selectChange && typeof(this.selectChange) === 'function') {
+            this.selectChange(data);
+          }
+        });
+        return;
+      }
       this.$on(`k-select-menu-update-current-${this._uid}`, (data) => {
         if (!data) return;
         this.$data.current = data;
@@ -78,6 +118,16 @@
       toggleSelect () {
         this.$data.selectShow = !this.$data.selectShow;
         SelectMenu(this);
+      },
+      handleMutSelected(data) {
+        let mutCurrentStr = '';
+        data.forEach((item, idx) => {
+          if (item.status) {
+            mutCurrentStr += item.text + '、';
+          }
+        });
+        mutCurrentStr = mutCurrentStr ? mutCurrentStr.substring(0, mutCurrentStr.length - 1) : '';
+        return mutCurrentStr;
       }
     }
   }
--- components/Select/SelectMenu.vue
+++ components/Select/SelectMenu.vue
@@ -1,6 +1,12 @@
 <template>
   <transition name="k-datepicker-picker-in">
-    <ul v-if="this.$options.input === 'readonly'" class="k k-absolute-menu k-select-menu" v-show="show">
+    <ul v-if="this.$options.type === 'mut'" class="k k-absolute-menu k-select-menu" v-show="show">
+      <li v-for="(each, idx) in select" @click="">
+        <div class="k-select-checkbox" @click.stop="mutSelect(each, idx)">
+          <i v-if="each.status" class="fa fa-check"></i>
+        </div>&nbsp;&nbsp;{{each.text}}
+      </li>
+    </ul><ul v-else-if="this.$options.input === 'readonly'" class="k k-absolute-menu k-select-menu" v-show="show">
       <li v-for="each in select" @click="selectIt(each.key)" :class="each.key === current.key ? 'k-select-active' : ''">{{each.text}}</li>
     </ul><ul v-else class="k k-absolute-menu k-select-menu" v-show="show">
       <li v-for="(each, idx) in selectArr" @click="inputSelectIt(each.key)">{{each.text}}</li>
@@ -38,11 +44,21 @@
       return {
         show: false,
         current: this.$options.current ? this.$options.current : this.$options.select[0],
-        select: this.$options.select ? this.$options.select : [],
-        selectArr: this.$options.select ? this.$options.select : []
+        select: this.$options.select ? this.$options.select : []
       }
     },
     methods: {
+      mutSelect(item, idx) {
+        this.$data.select[idx].status = !this.$data.select[idx].status;
+        const arr = [];
+        this.$data.select.forEach((item, idx) => {
+           if (item.status) {
+             arr.push(item);
+           }
+         });
+        this.$options.$emit(`k-select-menu-update-mut-select-${this.$options._uid}`, arr);
+        this.$forceUpdate();
+      },
       selectIt (key) {
         this.select.forEach((each) => {
           if (each.key === key) {
